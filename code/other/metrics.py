ba1104a31ede9181dae822bc7e0f8a74b1898c1c
tomaszzywicki <tomasz.zywicki03@gmail.com>
2024-06-04 20:47:25 +0200

    add metrics

--- code/other/metrics.py
+++ code/other/metrics.py
@@ -1,7 +1,7 @@
 from scipy.spatial import distance
 import numpy as np
 
-def count_clustering_scores(X, cluster_num, model, score_fun):
+def count_clustering_scores(X, cluster_num, model_instance, score_fun):
     if isinstance(cluster_num, int):
         cluster_num_iter = [cluster_num]
     else:
@@ -9,7 +9,6 @@ def count_clustering_scores(X, cluster_num, model, score_fun):
         
     scores = []    
     for k in cluster_num_iter:
-        model_instance = model(n_clusters=k)
         labels = model_instance.fit_predict(X)
         wcss = score_fun(X, labels)
         scores.append(wcss)
@@ -25,11 +24,11 @@ def min_interclust_dist(X, label):
     clusters = set(label)
     global_min_dist = np.inf
     for cluster_i in clusters:
-        cluster_i_idx = np.where(label == cluster_i)
+        cluster_i_idx = np.where(label == cluster_i)[0].tolist()
         for cluster_j in clusters:
             if cluster_i != cluster_j:
-                cluster_j_idx = np.where(label == cluster_j)
-                interclust_min_dist = np.min(distance.cdist(X[cluster_i_idx], X[cluster_j_idx]))
+                cluster_j_idx = np.where(label == cluster_j)[0].tolist()
+                interclust_min_dist = np.min(distance.cdist(X.iloc[cluster_i_idx], X.iloc[cluster_j_idx]))
                 global_min_dist = np.min([global_min_dist, interclust_min_dist])
     return global_min_dist
 
@@ -37,8 +36,8 @@ def _inclust_mean_dists(X, label):
     clusters = set(label)
     inclust_dist_list = []
     for cluster_i in clusters:
-        cluster_i_idx = np.where(label == cluster_i)
-        inclust_dist = np.mean(distance.pdist(X[cluster_i_idx]))
+        cluster_i_idx = np.where(label == cluster_i)[0].tolist()
+        inclust_dist = np.mean(distance.pdist(X.iloc[cluster_i_idx]))
         inclust_dist_list.append(inclust_dist)
     return inclust_dist_list
 
@@ -54,18 +53,20 @@ def mean_dist_to_center(X, label):
     clusters = set(label)
     inclust_dist_list = []
     for cluster_i in clusters:
-        cluster_i_idx = np.where(label == cluster_i)
-        cluster_i_mean = np.mean(X[cluster_i_idx], axis=0, keepdims=True)
-        inclust_dist = np.mean(distance.cdist(X[cluster_i_idx], cluster_i_mean))
+        cluster_i_idx = np.where(label == cluster_i)[0].tolist()
+        cluster_i_mean = np.mean(X.iloc[cluster_i_idx], axis=0)
+        inclust_dist = np.mean(distance.cdist(X.iloc[cluster_i_idx], cluster_i_mean))
         inclust_dist_list.append(inclust_dist)
     return np.mean(inclust_dist_list)
 
-def print_metrics(X, cluster_num, model):
+def print_metrics(X, cluster_num, model_instance):
     print(f'Minimal distance between clusters = '
-          f'{count_clustering_scores(X, cluster_num, model, min_interclust_dist):.2f}.')
+          f'{count_clustering_scores(X, cluster_num, model_instance, min_interclust_dist):.2f}.')
     print(f'Average distance between points in the same class = '
-          f'{count_clustering_scores(X, cluster_num, model, mean_inclust_dist):.2f}.')
+          f'{count_clustering_scores(X, cluster_num, model_instance, mean_inclust_dist):.2f}.')
     print(f'Standard deviation of distance between points in the same class = '
-            f'{count_clustering_scores(X, cluster_num, model, std_dev_of_inclust_dist):.3f}.')
-    print(f'Average distance to cluster center = '
-            f'{count_clustering_scores(X, cluster_num, model, mean_dist_to_center):.2f}.')
+            f'{count_clustering_scores(X, cluster_num, model_instance, std_dev_of_inclust_dist):.3f}.')
+    # print(f'Average distance to cluster center = '
+    #         f'{count_clustering_scores(X, cluster_num, model_instance, mean_dist_to_center):.2f}.')
+
+    # chuj nie działa ta czwarta a nie chce mi się teraz 2h spędzić nad tym bo się trzeba na kolosy uczyć xd
